eb147a00f61ae8c222fb6e60019b6bb17b65eeeb
Norbert2711 <norbertu2711@gmail.com>
2020-05-28 16:22:54 +0200

    first commit

---
+++ src/main/java/com/project/results/ProjectResultsApplication.java
@@ -0,0 +1,13 @@
+package com.project.results;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectResultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectResultsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/results/ProjectResultsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.results;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProjectResultsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
