bf2886bb7918467464329d26f4fff798b1db0ef7
Norbert2711 <norbertu2711@gmail.com>
2021-01-20 14:08:29 +0100

    auto conf

--- src/main/java/com/project/results/ProjectResultsApplication.java
+++ src/main/java/com/project/results/ProjectResultsApplication.java
@@ -2,9 +2,10 @@ package com.project.results;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ProjectResultsApplication{
 
 	public static void main(String[] args) {
