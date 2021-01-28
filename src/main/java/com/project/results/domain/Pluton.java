3b178943ab36bb279f594373ff18c1c2456a023c
Norbert2711 <norbertu2711@gmail.com>
2021-01-28 17:41:33 +0100

    Completed heroku

--- src/main/java/com/project/results/domain/Pluton.java
+++ src/main/java/com/project/results/domain/Pluton.java
@@ -1,11 +1,6 @@
 package com.project.results.domain;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.List;
 
